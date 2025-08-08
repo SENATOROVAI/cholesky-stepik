9b5ab7c536e6b6de471b90bd4ab9ba40a594c66a
Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>
2025-08-08 04:35:35 +0300

    Create task2_cholesky.py
    Signed-off-by: Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>

---
+++ task2_cholesky.py
@@ -0,0 +1,12 @@
+# Solving Ax = b using Cholesky
+A = np.array([[ЧИСЛА],
+              [ЧИСЛА],
+              [ЧИСЛА]])
+b = np.array([ЧИСЛА])
+
+# Cholesky Decomposition
+
+# Forward substitution: L * y = b
+linalg.solve()
+
+# Backward substitution: L.T * x = y
