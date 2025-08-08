77e89d16b2e9b71a79f4a914a4534816f874003a
Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>
2025-08-08 03:08:33 +0300

    Create task1_lu.py
    Signed-off-by: Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>

---
+++ task1_lu.py
@@ -0,0 +1,13 @@
+def lu(A):
+    n = A.shape[0]
+    U = A.copy()
+    L = np.eye(n, dtype=np.double)
+    for i in range(n):
+        factor = U[i+1:, i] / U[i, i]
+        L[i+1:, i] = factor
+        U[i+1:] -= factor[:, np.newaxis] * U[i]
+    return L, U
+
+A = np.array([[ЧИСЛА], [ЧИСЛА], [ЧИСЛА]], dtype='float')
+L, U = 
+np.allclose(A, L УМНОЖИТЬ U)
